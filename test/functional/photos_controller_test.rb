ed49d85d3ca693464a07ac8223c94d10917ebd0e
Mike Bull <mbull959@gmail.com>
2014-03-16 17:51:51 -0700

    add new tests for photos, increase text area on photo form

--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class PhotosControllerTest < ActionController::TestCase
+  setup do
+    @photo = photos(:one)
+    @user = users(:one)
+  end
 
   test "gets index" do
     get :index
@@ -9,6 +13,21 @@ class PhotosControllerTest < ActionController::TestCase
 
   test "gets photo gallery" do
     get :photo_gallery
+    assert :success
+  end
+
+  test "gets new" do
+    get :new
+    assert :success
+  end
+
+  test "posts to create" do
+    post :create, photo: @photo.attributes
+    assert_redirected_to photo_path(assigns(:photo))
+  end
+
+  test "gets edit" do
+    get :edit, id: @photo.id
     assert :success
   end
 
